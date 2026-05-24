a146e65fa10f9112b443eff0c60ae560ba652686
spandeau <kossijire@gmail.com>
2026-05-24 12:51:44 +0000

    feat: homepage complete - pret pour deploiement

--- frontend-nextjs/next.config.ts
+++ frontend-nextjs/next.config.ts
@@ -1,11 +1,21 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
       },
     ],
   },
