b0e180f017b86cfafd4bc62af5ebe56ef5bb2231
spandeau <kossijire@gmail.com>
2026-06-18 21:47:57 +0000

    fix: supprime ActualitesPage.tsx dans actualites/slug

--- frontend-nextjs/src/app/actualites/[slug]/ActualitesPage.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllArticles } from "@/lib/articles";
-import { ActualitesClient } from "./page";
-
-export default async function ActualitesPage() {
-  const articles = await getAllArticles();
-  return <ActualitesClient articles={articles} />;
-}
